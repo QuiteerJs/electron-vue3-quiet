6bef80b627ed97ca2edc17349cbd35bba6193f63
aimin <528627554@qq.com>
2022-07-01 11:33:31 +0800

    VITE配置

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, loadEnv } from 'vite'
+import { resolveConfig, viteDefine, resolvePath, setupVitePlugins, electronEngine } from './build'
+import { resolve } from 'path'
+
+export default defineConfig(configEnv => {
+  const { root, renderer } = resolvePath('./', import.meta.url)
+  const viteEnv = loadEnv(configEnv.mode, process.cwd()) as ImportMetaEnv
+
+  return {
+    base: './',
+    root: renderer,
+    resolve: resolveConfig(renderer),
+    define: viteDefine,
+    build: {
+      outDir: resolve(root, 'dist'),
+      target: 'esnext',
+      minify: 'esbuild',
+      brotliSize: false,
+      emptyOutDir: false,
+      chunkSizeWarningLimit: 2000
+    },
+    server: {
+      host: '0.0.0.0'
+    },
+    plugins: [...setupVitePlugins(configEnv, renderer), electronEngine(configEnv, viteEnv)],
+    publicDir: resolve(root, 'public')
+  }
+})
