54632e6bf29defb26c4d666ca7cdbdb613c1fca1
zam <528627554@qq.com>
2022-07-31 22:56:43 +0800

    feat(types): 使用工具类增加返回值类型

--- src/preload/main/index.ts
+++ src/preload/main/index.ts
@@ -1,10 +1,14 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
+type IpcParameters<K extends keyof typeof Electron.ipcRenderer> = Parameters<typeof Electron.ipcRenderer[K]>
+type IpcReturnType<K extends keyof typeof Electron.ipcRenderer> = ReturnType<typeof Electron.ipcRenderer[K]>
+
 contextBridge.exposeInMainWorld('$ipc', {
-  send: (...args: Parameters<typeof Electron.ipcRenderer['send']>): void => ipcRenderer.send(...args),
-  sendSync: (...args: Parameters<typeof Electron.ipcRenderer['send']>) => ipcRenderer.sendSync(...args),
-  invoke: (...args: Parameters<typeof Electron.ipcRenderer['invoke']>) => ipcRenderer.invoke(...args),
-  on: (...args: Parameters<typeof Electron.ipcRenderer['on']>) => ipcRenderer.on(...args),
-  once: (...args: Parameters<typeof Electron.ipcRenderer['once']>) => ipcRenderer.once(...args),
-  removeAllListeners: (channel: string) => ipcRenderer.removeAllListeners(channel)
+  send: (...args: IpcParameters<'send'>): IpcReturnType<'send'> => ipcRenderer.send(...args),
+  sendSync: (...args: IpcParameters<'sendSync'>): IpcReturnType<'sendSync'> => ipcRenderer.sendSync(...args),
+  invoke: (...args: IpcParameters<'invoke'>): IpcReturnType<'invoke'> => ipcRenderer.invoke(...args),
+  on: (...args: IpcParameters<'on'>): IpcReturnType<'on'> => ipcRenderer.on(...args),
+  once: (...args: IpcParameters<'once'>): IpcReturnType<'once'> => ipcRenderer.once(...args),
+  removeAllListeners: (...args: IpcParameters<'removeAllListeners'>): IpcReturnType<'removeAllListeners'> =>
+    ipcRenderer.removeAllListeners(...args)
 })
