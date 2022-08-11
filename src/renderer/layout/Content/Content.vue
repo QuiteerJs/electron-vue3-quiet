817c5de5718766106779429e59d913489beacffc
quiteer <p602985@yeah.net>
2022-08-11 21:24:47 +0800

    build(deps): 更新依赖

--- src/renderer/layout/Content/Content.vue
+++ src/renderer/layout/Content/Content.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="content" a:bg="base">
-    <div a:text="30px primary" a:p="b-20px" a:bg="info-active">primary</div>
+    <!-- <div a:text="30px primary" a:p="b-20px" a:bg="info-active">primary</div>
     <div a:text="30px info" a:bg="light">info</div>
     <div a:text="30px success" a:bg="light">success</div>
     <div a:text="30px warning" a:bg="light">warning</div>
     <div a:text="30px error" a:bg="light">error</div>
-    <div a:text="30px primary" a:p="b-20px" a:bg="*test">别名</div>
+    <div a:text="30px primary" a:p="b-20px" a:bg="*test">别名</div> -->
     <RouterView />
   </div>
 </template>
